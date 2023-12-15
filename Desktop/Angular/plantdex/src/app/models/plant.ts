cd1206dd8dc93116688ed02f22167690d5920a34
Kristina Slavkovic <137336285+Ollysheep@users.noreply.github.com>
2023-12-15 12:38:23 +0100

    4 - affichage plant + service

---
+++ Desktop/Angular/plantdex/src/app/models/plant.ts
@@ -0,0 +1,8 @@
+export interface Plant {
+  arrosage: number;
+  categorie: string;
+  id: number;
+  image: string;
+  nom: string;
+  soleil: string;
+}
--- Desktop/Angular/plantdex/src/app/pages/page-home/page-home.component.ts
+++ Desktop/Angular/plantdex/src/app/pages/page-home/page-home.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Plant } from 'src/app/models/plant';
+import { PlantsService } from 'src/app/services/plants.service';
 
 @Component({
   selector: 'app-page-home',
   templateUrl: './page-home.component.html',
-  styleUrls: ['./page-home.component.css']
+  styleUrls: ['./page-home.component.css'],
 })
-export class PageHomeComponent {
+export class PageHomeComponent implements OnInit {
+  plantsToDisplay: Plant[] = [];
+  constructor(private plantsService: PlantsService) {}
+  ngOnInit(): void {
+    console.log(this.plantsService.getPlants());
 
+    this.plantsService.getPlants().subscribe((data) => {
+      console.log(data);
+      this.plantsToDisplay = [...data];
+    });
+  }
 }
